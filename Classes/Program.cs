5e788b7507cb57a49f367cf368a8f6cec1bb1058
memcmahon <mcmahon.meganelizabeth@gmail.com>
2022-02-04 09:43:39 -0700

    Section 2.1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string otherName)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", otherName, Name);
+        }
+
+        public static Person Parse(string name)
+        {
+            var person = new Person();
+            person.Name = name;
+
+            return person;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = Person.Parse("Megan");
+
+            person.Introduce("Mosh");
+            
         }
     }
 }
